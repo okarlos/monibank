e89e59ae55cecbf0e260529b6c21a639dd057b65
Karlos Kliemann <karloskliemann@gmail.com>
2023-05-28 07:03:36 -0300

    feat: end cpf validation

--- js/validaCPF.js
+++ js/validaCPF.js
@@ -1,7 +1,10 @@
 export default function validaCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    console.log(validaNumerosRepetidos(cpf));
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("CPF não existe!")
+    } else {
+        console.log("Existe!")
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -37,4 +40,22 @@ function validaPrimeiroDigito(cpf) {
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+    
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
